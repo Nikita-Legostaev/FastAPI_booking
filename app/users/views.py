c1f1587cd28b751fbd449a1ac304d54df40bad30
login <nikitooo111333222@gmail.com>
2024-07-21 12:07:10 +0300

    update CRUD users

--- app/users/views.py
+++ app/users/views.py
@@ -17,40 +17,37 @@ async def get_all_users(user: Users = Depends(get_current_user)):
     return await UserRepository.get_all()
 
 
-@router.patch("/{user_id}/email")
+@router.patch("/change/email")
 async def change_email_user(
-    user_id: int, 
     new_email: str,
     user: Users = Depends(get_current_user)
     ):
-    existing_user = await UserRepository.find_one_or_none(id=user_id)
+    existing_user = await UserRepository.find_one_or_none(id=user.id)
     if not existing_user:
         raise HTTPException(status_code=404, detail="User not found")
     if existing_user.email == new_email:
         raise HTTPException(status_code=400, detail="Email is already in use")
 
-    await UserRepository.update(user_id, email = new_email)
+    await UserRepository.update(user.id, email = new_email)
     return {"detail": "Email updated successfully"}
 
-@router.patch("/{user_id}/password")
-async def change_password_user(
-    user_id: int, 
+@router.patch("/change/password")
+async def change_password_user( 
     new_password: str, 
     user: Users = Depends(get_current_user)
     ):
-    existing_user = await UserRepository.find_one_or_none(id=user_id)
+    existing_user = await UserRepository.find_one_or_none(id=user.id)
     if not existing_user:
         raise HTTPException(status_code=404, detail="User not found")
     hashed_password = get_password_hash(new_password)
-    await UserRepository.update(user_id, hash_password = hashed_password)
+    await UserRepository.update(user.id, hash_password = hashed_password)
     
 
-@router.delete("/{user_id}")
+@router.delete("/delete")
 async def delete_user(
-    user_id: int, 
     user: Users = Depends(get_current_user)
     ):
-    existing_user = await UserRepository.find_one_or_none(id=user_id)
+    existing_user = await UserRepository.find_one_or_none(id=user.id)
     if not existing_user:
         raise HTTPException(status_code=404, detail="User not found")
-    await UserRepository.delete(id=user_id)
+    await UserRepository.delete(id=user.id)
